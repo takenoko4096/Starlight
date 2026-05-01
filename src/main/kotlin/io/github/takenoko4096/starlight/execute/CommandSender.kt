7055c450b07fad48acb458998473e331ec4f3e50
topaz <takenokoii78@gmail.com>
2026-05-01 16:24:20 +0900

    a

--- src/main/kotlin/io/github/takenoko4096/starlight/execute/CommandSender.kt
+++ src/main/kotlin/io/github/takenoko4096/starlight/execute/CommandSender.kt
@@ -1,6 +1,15 @@
 package io.github.takenoko4096.starlight.execute
 
+import io.github.takenoko4096.starlight.math.Vector3d
 import net.minecraft.core.HolderLookup
+import net.minecraft.world.entity.Entity
 
-class CommandSender(val registryAccess: HolderLookup.Provider) {
+sealed class CommandSender(val registryAccess: HolderLookup.Provider) {
+    abstract fun getPosition(): Vector3d
+
+    class EntityCommandSender(registryAccess: HolderLookup.Provider, val entity: Entity) : CommandSender(registryAccess) {
+        override fun getPosition(): Vector3d {
+            return Vector3d.from(entity.position())
+        }
+    }
 }
--- src/main/kotlin/io/github/takenoko4096/starlight/execute/CommandSourceStack.kt
+++ src/main/kotlin/io/github/takenoko4096/starlight/execute/CommandSourceStack.kt
@@ -2,34 +2,31 @@ package io.github.takenoko4096.starlight.execute
 
 import io.github.takenoko4096.starlight.math.Rotation2f
 import io.github.takenoko4096.starlight.math.Vector3d
-import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.core.registries.Registries
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.level.Level
 
 class CommandSourceStack(sender: CommandSender) {
-    private var executor: Entity?
+    private var executor: Entity? = null
 
-    private var position: Vector3d
+    private var position: Vector3d = Vector3d()
 
-    private var rotation: Rotation2f
+    private var rotation: Rotation2f = Rotation2f()
 
-    private var dimension: Level
+    private var dimension: Level = sender.registryAccess
+        .lookupOrThrow(Registries.DIMENSION)
+        .getOrThrow(Level.OVERWORLD)
+        .value()
 
-    private var entityAnchor: EntityAnchor
+    private var entityAnchor: EntityAnchor = EntityAnchor.FEET
 
     init {
-        executor = null
-        position = Vector3d()
-        rotation = Rotation2f()
-        dimension = sender.registryAccess
-            .lookupOrThrow(Registries.DIMENSION)
-            .getOrThrow(Level.OVERWORLD)
-            .value()
-        entityAnchor = EntityAnchor.FEET
+
     }
 
-    fun clone() {
+    fun clone(): CommandSourceStack {
+        return CommandSourceStack(
 
+        )
     }
 }
